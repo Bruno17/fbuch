8602974bcee7628e8451f339b00fa75426f9b1a2
Bruno17 <b.perner@gmx.de>
2021-01-23 13:42:31 +0100

    pegel individualisieren

--- core/components/fbuch/elements/snippets/fbuch_getpegel.snippet.php
+++ core/components/fbuch/elements/snippets/fbuch_getpegel.snippet.php
@@ -1,9 +1,14 @@
 <?php
-$url = 'https://www.pegelonline.wsv.de/webservices/rest-api/v2/stations/steinbach/W.json?includeCurrentMeasurement=true';
+$show_pegel = $modx->getOption('show_pegel',$scriptProperties,'1');
+$station = $modx->getOption('station',$scriptProperties,'');
+$station_name = $modx->getOption('station_name',$scriptProperties,'');
+$max_pegel = (int) $modx->getOption('max_pegel',$scriptProperties,'400');//maximal angezeigter Pegel = 100%
+$max_pegel_ok = (int) $modx->getOption('max_pegel_ok',$scriptProperties,'200');//Oberkante Notsteg
+$pegel_danger = (int) $modx->getOption('pegel_danger',$scriptProperties,'240');//Unterkante Warnschild
 
-$show_pegel = $modx->getOption('show_pegel');
+$url = 'https://www.pegelonline.wsv.de/webservices/rest-api/v2/stations/' . $station . '/W.json?includeCurrentMeasurement=true';
 
-if (empty($show_pegel)){
+if (empty($station) || empty($show_pegel)){
     return '';
 }
 
@@ -27,16 +32,16 @@ $data = json_decode($result, true);
 
 $current_pegel = $data['currentMeasurement']['value'];
 
-$pegel_ok_notsteg = 200;//Oberkante Notsteg = 50%
-$pegel_uk_warnschild = 240;//Unterkante Warnschild
-$pegel_max = 400;//maximal angezeigter Pegel = 100%
+//$mm_percent = 0.25;//1cm entspricht 0,25%
 
-$mm_percent = 0.25;//1cm entspricht 0,25%
+$current_pegel_percent = ($current_pegel * 100) /  $max_pegel;
+$max_pegel_ok_percent = ($max_pegel_ok * 100) /  $max_pegel; 
+$pegel_danger_percent = ($pegel_danger * 100) /  $max_pegel;  
 
-$properties = array();
+$properties = $scriptProperties;
 $properties['gp_current_pegel'] = $current_pegel;
-$properties['gp_pegel_percent'] = str_replace(',','.',$current_pegel*$mm_percent);
+$properties['gp_pegel_percent'] = str_replace(',','.',$current_pegel_percent);
+$properties['gp_max_pegel_ok_percent'] = str_replace(',','.',$max_pegel_ok_percent);
 $properties['gp_pegel_percent_rest'] = str_replace(',','.',100-($current_pegel*$mm_percent));
-$properties['gp_pegel_uk_warnschild_percent'] = $pegel_uk_warnschild*$mm_percent;
-
+$properties['gp_pegel_danger_percent'] = str_replace(',','.',($pegel_danger_percent));
 return $modx->getChunk($tpl,$properties);
