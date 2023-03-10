05770dad27fc76f3737e3abe3b97c793f1272345
Bruno17 <b.perner@gmx.de>
2023-03-10 14:18:32 +0100

    snippet fbuchGetAssetsPath

--- core/components/fbuch/elements/snippets/fbuchgetassetsfiles.snippet.php
+++ core/components/fbuch/elements/snippets/fbuchgetassetsfiles.snippet.php
@@ -3,8 +3,12 @@ $folder = $modx->getOption('folder',$scriptProperties,'');
 $pattern = $modx->getOption('pattern',$scriptProperties,'*.html');
 $wrapper = $modx->getOption('wrapper',$scriptProperties,'[[+output]]');
 
+$assets_path = $modx->getOption('fbuch.assets_path',null,$modx->getOption('assets_path').'components/fbuch/');
+
 $output = '';
-foreach (glob($folder . "/" . $pattern) as $filename) {
+$files = glob($assets_path . $folder . "/" . $pattern);
+
+foreach ($files as $filename) {
     $fileinfo = pathinfo($filename);
     $newwrapper = str_replace('[[+filename]]',$fileinfo['filename'],$wrapper);
     $output .= str_replace('[[+output]]',file_get_contents($filename),$newwrapper) . "\n";
