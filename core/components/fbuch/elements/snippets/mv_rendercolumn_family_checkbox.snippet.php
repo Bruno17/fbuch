6be7b94665686353acbc281c57e8f966f0190996
Bruno17 <b.perner@gmx.de>
2025-01-08 07:16:04 +0100

    beispielhafte Kompetenzstufen importieren

--- core/components/fbuch/elements/snippets/mv_rendercolumn_family_checkbox.snippet.php
+++ core/components/fbuch/elements/snippets/mv_rendercolumn_family_checkbox.snippet.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $object_id = $modx->getOption('object_id',$_REQUEST,0);
 $req_configs = $modx->getOption('reqConfigs',$_REQUEST,0);
 if ($req_configs == 'mv_families:fbuch'){
@@ -10,16 +10,21 @@ if ($req_configs == 'mv_families:fbuch'){
 }
 $m_family_id = $modx->getOption('family_id',$scriptProperties,0);
 
+$assetsUrl = $modx->getOption('migx.assets_url');
+if (empty($assetsUrl)) {
+    $assetsUrl = $modx->getOption('assets_url') . 'components/migx/' ;    
+}
+
 $output= '
 <a href="#">
-<img title="unchecked" alt="unchecked" src="/assets/components/migx/style/images/cb_empty.png" class="controlBtn unchecked this.handleColumnSwitch familymember:1">
+<img title="unchecked" alt="unchecked" src="' . $assetsUrl . 'style/images/cb_empty.png" class="controlBtn unchecked this.handleColumnSwitch familymember:1">
 </a>
 ';
 
 if (!empty($o_family_id) && $m_family_id == $o_family_id){
 $output= '
 <a href="#">
-<img title="checked" alt="checked" src="/assets/components/migx/style/images/cb_ticked.png" class="controlBtn checked this.handleColumnSwitch familymember:0">
+<img title="checked" alt="checked" src="' . $assetsUrl . 'style/images/cb_ticked.png" class="controlBtn checked this.handleColumnSwitch familymember:0">
 </a>
 ';
 }
